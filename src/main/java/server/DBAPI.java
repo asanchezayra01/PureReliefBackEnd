888f1540b7e59d0d6ce6d0ee4f643a0c752f442a
asanc <asanc375@fiu.edu>
2020-02-01 05:31:43 -0500

    Pushing in jdbc and DBAPI

---
+++ src/main/java/server/DBAPI.java
@@ -0,0 +1,56 @@
+package server;
+
+import java.sql.*;
+
+import org.json.JSONObject;
+
+
+public class DBAPI {
+	
+	private Connection connect;
+	
+	final private String password = "PureRelief1";
+	
+	public DBAPI() throws Exception
+	{
+		try
+		{
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			
+			// Change this to a const.
+			connect = DriverManager
+					.getConnection("jdbc:mysql://10.136.104.49:3306/?user=root&password=" + password);
+		}
+		catch(Exception ex)
+		{
+			throw new Exception("Failed to connect to database.\nMore details: " + ex.getMessage());
+		}
+	}
+	
+	
+	public String registerShelter(JSONObject json) throws Exception
+	{
+		String out = "";
+		
+		try
+		{
+		final String query = "CALL pure_relief.register_shelter(?,?)";
+		
+		CallableStatement procedure = connect.prepareCall(query);
+		
+		procedure.setString(1, json.getString("userName"));
+		procedure.setString(2, json.getString("password"));
+		
+		procedure.execute();
+		
+		return out= "SUCCESS";
+		}
+		catch(Exception ex)
+		{
+			throw ex;
+		}
+	}
+	
+	
+
+}
